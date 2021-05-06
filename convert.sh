d04cfa9ceac1e39d59f6db239e1493d4e08e6447
Omikhleia <didier.willis@gmail.com>
2021-05-07 01:19:18 +0200

    Files renaming

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xsltproc tei2html/tei-lite.xsl src/dict-sd-fr-de-en_2.0a5x.xml \
+xsltproc tei2html/tei-lite.xsl src/dict-sd-fr-en.xml \
   | node tei2html/post-process.js \
-  > build/dict-sd.html
+  > docs/dict-sd.html
 
