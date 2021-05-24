e8c582079c4406762b0736c6522d4c4098309951
Omikhleia <didier.willis@gmail.com>
2021-05-24 12:34:42 +0200

    Clean-up: reorganize processing scripts

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xsltproc tei2html/tei-lite.xsl src/dict-sd-fr-en.xml \
-  | node tei2html/post-process.js \
+xsltproc scripts/tei2html/tei-lite.xsl src/dict-sd-fr-en.xml \
+  | node scripts/tei2html/post-process.js \
   > docs/dict-sd.html
 
--- process.sh
+++ process.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-xsltproc --novalid xslt/process/expand-xref.xsl src/dict-sd-fr-en.xml \
-  | xsltproc --novalid xslt/process/expand-xref-2.xsl - \
-  | xsltproc --novalid xslt/process/sort.xsl - \
-  | xsltproc --novalid xslt/process/expand-renum.xsl - \
-  | xsltproc --novalid xslt/process/add-sections.xsl - \
-  | xsltproc tei2html/tei-lite.xsl - \
-  | node tei2html/post-process.js \
+xsltproc --novalid scripts/process/expand-xref.xsl src/dict-sd-fr-en.xml \
+  | xsltproc --novalid scripts/process/expand-xref-2.xsl - \
+  | xsltproc --novalid scripts/process/sort.xsl - \
+  | xsltproc --novalid scripts/process/expand-renum.xsl - \
+  | xsltproc --novalid scripts/process/add-sections.xsl - \
+  | xsltproc scripts/tei2html/tei-lite.xsl - \
+  | node scripts/tei2html/post-process.js \
   > docs/dict-sd.html
